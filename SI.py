67ec972ece471a7c672d435a561e093cfc1d6e36
mannat manocha <mannatmanocha06@gmail.com>
2023-01-30 11:32:51 +0530

    first commit

---
+++ SI.py
@@ -0,0 +1,10 @@
+# SI calculator
+p=10000
+r=0.5
+t=10
+si=p*r*t/100
+print(f'Principle is{p}')
+print(f'Rate is{r}%')
+print(f'Time is{t}yrs')
+print('-----'*10)
+print(f'Simple Interest is{si}')
---
+++ hyp.py
@@ -0,0 +1,8 @@
+perp=12
+base=5
+hyp=(perp**2+base**2)**.5
+print('PYTHAGOREAN THEOREM')
+print(f'Perpendicular is:{perp}')
+print(f'Base is:{base}')
+print('---'*10)
+print(f'Hypotenuse is:{hyp}')
